19ebfd2a67ea64413721b22e0f37e1032cf80a7d
WebMaster <webmaster@WebMasters-Mac.local>
2017-04-04 06:59:42 +0300

    modify routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "home#index"
+  # root "home#index"
 
   mount API => '/'
   mount Sidekiq::Web => '/sidekiq'
-
+  root to: "admin/dashboard#index"
 end
