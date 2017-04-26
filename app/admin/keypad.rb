44698e0c27fcbb69ca0781b379363372375319e2
WebMaster <webmaster@WebMasters-Mac.local>
2017-04-26 17:16:10 +0300

    modify sign_in_after_link

--- app/admin/keypad.rb
+++ app/admin/keypad.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Keypad, as: "Keypad" do
   permit_params :number, :password, :code, :status, :admin_id
 
   controller do
-    
+
   end
 
   index do
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,10 @@ class ApplicationController < ActionController::Base
   def access_denied(exception)
     redirect_to new_admin_keypad_path, alert: exception.message
   end
+
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(AdminUser)
+      new_admin_keypad_path
+    end
+  end
 end
