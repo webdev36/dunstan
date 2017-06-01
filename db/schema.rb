a8e16bf595cb232bf25edc43cf2489b5cf46486a
WebMaster <webmaster@WebMasters-Mac.local>
2017-06-01 22:16:55 +0300

    added sim_mumber to keypad model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170601184252) do
+ActiveRecord::Schema.define(version: 20170601184254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 20170601184252) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.string   "theme_number"
+    t.string   "sim_number"
   end
 
   create_table "roles", force: :cascade do |t|
